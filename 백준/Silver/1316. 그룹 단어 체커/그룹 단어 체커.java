d04114ef8ad848b5a2208f3c7ab2bdcc57c7b10a
seyeonham <seyeon021023@gmail.com>
2026-03-02 23:03:57 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 104 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int count = n;
+
+        for (int i = 0; i < n; i++) {
+            boolean[] arr = new boolean[26];
+            String s = br.readLine();
+            
+            if (s.length() > 0) {
+                arr[s.charAt(0) - 97] = true;
+            }
+
+            for (int j = 1; j < s.length(); j++) {
+                char c = s.charAt(j);
+                if (c == s.charAt(j-1)) {
+                    continue;
+                }
+                if (arr[(int)c - 97]) {
+                    count--;
+                    break;
+                }
+                arr[(int)c - 97] = true;
+            }
+        }
+
+        System.out.print(count);
+    }
+}
