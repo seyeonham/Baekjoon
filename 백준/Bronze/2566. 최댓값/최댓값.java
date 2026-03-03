287eee1ab8308ecf18408c66e747cb05fa6588d3
seyeonham <seyeon021023@gmail.com>
2026-03-03 15:54:46 +0900

    [Bronze III] Title: 최댓값, Time: 124 ms, Memory: 15948 KB -BaekjoonHub

---
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int[][] arr = new int[9][9];
+        int max = 0;
+        int a = 1;
+        int b = 1;
+
+        for (int i = 0; i < 9; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 9; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+                if (arr[i][j] > max) {
+                    max = arr[i][j];
+                    a = i + 1;
+                    b = j + 1;
+                }
+            }
+        }
+
+        System.out.println(max);
+        System.out.println(a + " " + b);
+    }
+}
