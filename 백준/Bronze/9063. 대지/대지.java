0cc4ef79dd1b501cbf8e4d5c553284dff0ba4fe2
seyeonham <seyeon021023@gmail.com>
2026-03-04 22:33:51 +0900

    [Bronze III] Title: 대지, Time: 252 ms, Memory: 27284 KB -BaekjoonHub

---
+++ 백준/Bronze/9063. 대지/대지.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int minX = Integer.parseInt(st.nextToken());
+        int maxX = minX;
+        int minY = Integer.parseInt(st.nextToken());
+        int maxY = minY;
+
+        for (int i = 0; i < n - 1; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            minX = Math.min(minX, x);
+            maxX = Math.max(maxX, x);
+            minY = Math.min(minY, y);
+            maxY = Math.max(maxY, y);
+        }
+
+        int result = (maxX - minX) * (maxY - minY);
+        System.out.print(result);
+    }
+}
