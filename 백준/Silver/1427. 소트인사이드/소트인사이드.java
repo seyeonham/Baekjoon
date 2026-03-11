39fb8de0b2980f8d72cb6f6c1ea0ba808cac852b
seyeonham <seyeon021023@gmail.com>
2026-03-11 22:59:05 +0900

    [Silver V] Title: 소트인사이드, Time: 100 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        int[] arr = new int[s.length()];
+
+        for (int i = 0; i < s.length(); i++) {
+            arr[i] = s.charAt(i) - '0';
+        }
+
+        Arrays.sort(arr);
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            System.out.print(arr[i]);
+        }
+    }
+}
