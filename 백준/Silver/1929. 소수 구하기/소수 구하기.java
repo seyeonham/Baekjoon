718d99fba66fabec104aa2ce41bab349ff1a448b
seyeonham <seyeon021023@gmail.com>
2026-03-12 22:53:30 +0900

    [Silver III] Title: 소수 구하기, Time: 424 ms, Memory: 18268 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = n; i <= m; i++) {
+            if (isPrime(i)) {
+                sb.append(i).append('\n');
+            }
+        }
+
+        System.out.print(sb);
+    }
+
+    public static boolean isPrime(int num) {
+        if (num < 2) return false;
+
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
