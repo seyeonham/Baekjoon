5555c98da43614dc283cc65b35477b5a88ba4f78
seyeonham <seyeon021023@gmail.com>
2026-03-14 14:32:57 +0900

    [Silver V] Title: 창문 닫기, Time: 100 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long n = Long.parseLong(br.readLine());
+
+        int count = (int) Math.sqrt(n);
+
+        System.out.print(count);
+    }
+}
