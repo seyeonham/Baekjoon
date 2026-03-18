ffc5c5ee1a6d8b2d0b708385e8af5ded2293e413
seyeonham <seyeon021023@gmail.com>
2026-03-18 13:30:49 +0900

    [Silver III] Title: queuestack, Time: 544 ms, Memory: 59060 KB -BaekjoonHub

---
+++ 백준/Silver/24511. queuestack/queuestack.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] type = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            type[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Deque<Integer> deque = new ArrayDeque<>();
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            if (type[i] == 0) {
+                deque.addLast(num);
+            }
+        }
+
+        int m = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < m; i++) {
+            int x = Integer.parseInt(st.nextToken());
+
+            deque.addFirst(x);
+            sb.append(deque.pollLast()).append(' ');
+        }
+
+        System.out.print(sb);
+    }
+}
