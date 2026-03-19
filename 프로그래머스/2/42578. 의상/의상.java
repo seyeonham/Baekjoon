871c6d9bdb32ec7f130939dba4fd85e849b53ed9
seyeonham <seyeon021023@gmail.com>
2026-03-19 11:53:41 +0900

    [level 2] Title: 의상, Time: 0.15 ms, Memory: 83.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < clothes.length; i++) {
+            map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0) + 1);
+        }
+
+        int result = 1;
+        
+        for (int value : map.values()) {
+            result *= (value + 1);
+        }
+        
+        return result - 1;
+    }
+}
