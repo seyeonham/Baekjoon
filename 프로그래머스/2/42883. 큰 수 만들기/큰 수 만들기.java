65c294bb58220cdcc63b3d213baaeba9524d14c5
seyeonham <seyeon021023@gmail.com>
2026-03-19 13:03:21 +0900

    [level 2] Title: 큰 수 만들기, Time: 35.59 ms, Memory: 98.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String solution(String number, int k) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < number.length(); i++) {
+            char c = number.charAt(i);
+
+            while (sb.length() > 0 && sb.charAt(sb.length() - 1) < c && k > 0) {
+                sb.deleteCharAt(sb.length() - 1);
+                k--;
+            }
+
+            sb.append(c);
+        }
+
+        if (k > 0) {
+            sb.setLength(sb.length() - k);
+        }
+
+        return sb.toString();
+    }
+}
