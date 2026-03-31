5b5c367337b1c44668dc1a5247f07ffcc1db151e
seyeonham <seyeon021023@gmail.com>
2026-03-31 17:25:36 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 104 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        System.out.print(factorial(n));
+    }
+
+    public static long factorial(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
+}
