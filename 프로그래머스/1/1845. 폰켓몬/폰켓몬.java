9f09e00f3a06ec66565f07593fee4fed75b8e2e0
seyeonham <seyeon021023@gmail.com>
2026-04-01 22:56:52 +0900

    [level 1] Title: 폰켓몬, Time: 9.09 ms, Memory: 87.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            set.add(nums[i]);
+        }
+        
+        if (set.size() == nums.length / 2) {
+            return set.size();
+        } else if (set.size() < nums.length / 2) {
+            return set.size();
+        } else {
+            return nums.length / 2;
+        }
+    }
+}
