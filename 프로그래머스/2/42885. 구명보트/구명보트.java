3f5f5e8de51970ceb136c164bcce0c47babc16a5
seyeonham <seyeon021023@gmail.com>
2026-05-18 02:23:19 +0900

    [level 2] Title: 구명보트, Time: 6.13 ms, Memory: 58.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        Arrays.sort(people);
+        
+        int left = 0;
+        int right = people.length - 1;
+        int answer = 0;
+        
+        while (left <= right) {
+            if (people[left] + people[right] <= limit) {
+                left++;
+                right--;
+            } else {
+                right--;
+            }
+
+            answer++;
+        }
+
+        return answer;
+    }
+}
