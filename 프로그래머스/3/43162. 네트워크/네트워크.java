432e243d302a3bfb42d9cab443131a943cc75c47
seyeonham <seyeon021023@gmail.com>
2026-05-28 20:34:42 +0900

    [level 3] Title: 네트워크, Time: 0.20 ms, Memory: 62.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    boolean[] visited;
+    
+    public int solution(int n, int[][] computers) {
+        visited = new boolean[n];
+        int answer = 0;
+        
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                answer++;
+                dfs(i, n, computers);
+            }
+        }
+       
+        return answer;
+    }
+    
+    public void dfs(int current, int n, int[][] computers) {
+        visited[current] = true;
+        
+        for (int next = 0; next < n; next++) {
+            if (computers[current][next] == 1 && !visited[next]) {
+            dfs(next, n, computers);        
+            }
+        }
+    }
+}
